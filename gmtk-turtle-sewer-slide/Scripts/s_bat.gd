b390eab138a9e63c07fd1567c7a234160be5e95d
Harold Garner <hgarner213@gmail.com>
2025-08-02 19:08:11 -0500

    Add files via upload

---
+++ gmtk-turtle-sewer-slide/Scripts/s_bat.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@onready var path_move = $move
+@onready var path_Attack = $Attack
+@onready var move_anime = $Move/PathFollow2D/Move/AnimationPlayer
+@onready var attack_anime = $Attack/PathFollow2D/Attack/AnimationPlayer
